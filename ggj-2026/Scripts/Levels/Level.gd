0a669e5ffc55c33410b6ef78434fc181c96e6d5b
annacarini <carini.1771784@studenti.uniroma1.it>
2026-01-31 11:39:08 +0100

    inizio funzionamento livelli

---
+++ ggj-2026/Scripts/Levels/Level.gd
@@ -0,0 +1,39 @@
+class_name Level extends Node
+	
+static var levelName: String = "Level"
+
+const GRID_SIZE := 21
+const CELL_SIZE := 46
+const SCREEN_SIZE := 966  # opzionale, solo per controllo
+
+
+var grid := []
+
+func _ready():
+	init_grid()
+	for child in $Objs.get_children():
+		place_object(child)
+
+func init_grid():
+	grid.clear()
+	for y in GRID_SIZE:
+		var row := []
+		for x in GRID_SIZE:
+			row.append(null)
+		grid.append(row)
+
+func place_object(obj) -> bool:
+	obj.position = obj.pos * GRID_SIZE
+	return true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		for child in $Objs.get_children():
+			child.position = child.pos * GRID_SIZE
+			
+			
+			
+static func getName() -> String:
+	return levelName
