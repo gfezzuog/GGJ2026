035e7e741427528255e343d1aee7953076b5475a
AdrianoRean <127209495+AdrianoRean@users.noreply.github.com>
2026-01-31 18:39:03 +0100

    debug

--- ggj-2026/node_2d.gd
+++ ggj-2026/node_2d.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var pippo: LevelInfo = load("res://Resources/new_resource.tres")
+	var pippo: LevelInfo = load("res://Resources/level_resource.tres")
 	print(pippo.get_level_info(1))
 	pass # Replace with function body.
 
