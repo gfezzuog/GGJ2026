a6beb8b665bf19fdcd7a37980f8acb8143b38a49
AdrianoRean <127209495+AdrianoRean@users.noreply.github.com>
2026-01-31 18:52:44 +0100

    to Vector2

--- ggj-2026/node_2d.gd
+++ ggj-2026/node_2d.gd
@@ -4,7 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var pippo: LevelInfo = load("res://Resources/level_resource.tres")
-	print(pippo.get_level_info(1))
+	var pippi = pippo.get_level_info(1)
+	print(pippi["masks"])
 	pass # Replace with function body.
 
 
