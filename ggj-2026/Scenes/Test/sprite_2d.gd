5c428696ebe377ddc8cf5a400c5aa11db158816e
egiubell <edoardogiubellini@gmail.com>
2026-01-31 15:36:28 +0100

    Shader funge con piu aree
    daje

--- ggj-2026/Scenes/Test/sprite_2d.gd
+++ ggj-2026/Scenes/Test/sprite_2d.gd
@@ -1,38 +1,38 @@
 extends Sprite2D
 
 @onready var mat := material as ShaderMaterial
-var effect_active := true
-
-@export var area_path: NodePath
-@onready var area := get_node(area_path) as Node2D
-
 @export var areas: Array[Area2D]
+var effect_active := true
+const MAX_RECTS := 8
 
 func _process(_delta):
 
 	if Input.is_action_just_pressed("jump"):
-		if effect_active:
-			effect_active = false
-		else:
-			effect_active = true
-			
+		effect_active = !effect_active
 	if not effect_active:
 		mat.set_shader_parameter("hide", false)
+		mat.set_shader_parameter("rect_count", 0)
 		return
 
-	# Converto la posizione del Marker in UV
-	#var local = to_local(marker.global_position)
-	var local = to_local(area.global_position)
-	var uv = Vector2(
-		local.x / texture.get_size().x + 0.5,
-		local.y / texture.get_size().y + 0.5
-	)
+	var rect_data: Array[Vector4] = []
+	
+	for area in areas:
+		var shape = area.get_node("CollisionShape2D").shape
+		if not shape is RectangleShape2D:
+			continue
+		# Converto la posizione del Marker in UV
+		var local = to_local(area.global_position)
+		var center_uv = Vector2(
+			local.x / texture.get_size().x + 0.5,
+			local.y / texture.get_size().y + 0.5
+		)
 
 	# Convertiamo la dimensione in UV
-	var size_uv = area.get_child(0).shape.size / texture.get_size()
-	#var size_uv = Vector2(0, 0)
+		var size_uv = area.get_child(0).shape.size / texture.get_size()
+		rect_data.append(Vector4(
+			center_uv.x, center_uv.y, size_uv.x, size_uv.y
+		))
 
 	# Aggiorniamo lo shader
-	mat.set_shader_parameter("mask_center_uv", uv)
-	mat.set_shader_parameter("mask_size_uv", size_uv)
-	mat.set_shader_parameter("hide", true)
+	mat.set_shader_parameter("rect_count", rect_data.size())
+	mat.set_shader_parameter("rects", rect_data)
