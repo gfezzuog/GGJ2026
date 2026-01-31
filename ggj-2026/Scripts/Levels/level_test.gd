ca8bbce89d258cbe1ae183c936df20d5f122144e
gfezzuog <nanaki1307@gmail.com>
2026-01-31 13:20:13 +0100

    push pre merge jfabi

--- ggj-2026/Scripts/Levels/level_test.gd
+++ ggj-2026/Scripts/Levels/level_test.gd
@@ -7,13 +7,25 @@ const GRID_SIZE := 21
 const CELL_SIZE := 46
 const SCREEN_SIZE := 966  # opzionale, solo per controllo
 
-
+var mask := []
 var grid := []
 
 func _ready():
 	init_grid()
 	for child in $Objs.get_children():
 		place_object(child)
+	#mask = $Mask.get_mask_coords()
+	#change_mask()
+
+#func change_mask():
+	#for child in $Objs.get_children():
+		#print(mask)
+		#for i in range(mask.size()) :
+			#print(mask[i])
+			#if (mask[i] == child.pos):
+				#print("la Maskera ha trovato un oggetto\n")
+	
+	
 
 func init_grid():
 	grid.clear()
