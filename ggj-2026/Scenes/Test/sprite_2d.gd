a1c852b3cc89b153ddc285eb420129882f0d369c
egiubell <edoardogiubellini@gmail.com>
2026-01-31 03:34:53 +0100

    Shader funzionante con area
    Finalmenteeeeeeee

--- ggj-2026/Scenes/Test/sprite_2d.gd
+++ ggj-2026/Scenes/Test/sprite_2d.gd
@@ -1,33 +1,37 @@
 extends Sprite2D
 
-@export var marker_path: NodePath
-@onready var marker := get_node(marker_path) as Node2D
 @onready var mat := material as ShaderMaterial
+@export var area_path: NodePath
+@onready var area := get_node(area_path) as Node2D
 
-var effect_active = true
+var effect_active := true
+@export var rect_size := Vector2(50, 50) # grandezza della zona in pixel
 
 func _process(_delta):
+
 	if Input.is_action_just_pressed("jump"):
 		if effect_active:
 			effect_active = false
 		else:
 			effect_active = true
+			
 	if not effect_active:
 		mat.set_shader_parameter("hide", false)
 		return
-		
-	var rect := Rect2(
-		global_position - texture.get_size() * 0.5,
-		texture.get_size()
-	)
 
-	var touching := rect.has_point(marker.global_position)
-	mat.set_shader_parameter("hide", touching)
-
-	# opzionale: aggiorna il centro del cerchio al Marker
-	var local = to_local(marker.global_position)
+	# Converto la posizione del Marker in UV
+	#var local = to_local(marker.global_position)
+	var local = to_local(area.global_position)
 	var uv = Vector2(
 		local.x / texture.get_size().x + 0.5,
 		local.y / texture.get_size().y + 0.5
 	)
+
+	# Convertiamo la dimensione in UV
+	var size_uv = area.get_child(0).shape.size / texture.get_size()
+	#var size_uv = Vector2(0, 0)
+
+	# Aggiorniamo lo shader
 	mat.set_shader_parameter("mask_center_uv", uv)
+	mat.set_shader_parameter("mask_size_uv", size_uv)
+	mat.set_shader_parameter("hide", true)
