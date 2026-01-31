ed5d36923641bcaf957927f74b9aac0503a28b33
jfabi88 <jacopo.fabi8@gmail.com>
2026-01-31 12:15:34 +0000

    cambiamento maschera

--- ggj-2026/Scenes/Mask/mask.gd
+++ ggj-2026/Scenes/Mask/mask.gd
@@ -2,14 +2,8 @@ class_name  Mask extends TextureRect
 
 signal mask_dragged(value: bool, mask: Mask)
 
-## Gli array dentro la shape rappresentano le righe, i valori dentro gli array sono gli indici ddelle 
-## colonne in cui è attiva la mashera. Esempio di maschera(* dove c'è la maschera, 0 dove non c'è):
-## [0 * *]
-## [0 0 0]
-## [0 * 0]
-## La shape è quindi [[1,2], [], [1]]
-@export var shape: Array[Array] = []
-@export var coord_position: Vector2i = Vector2i(0, 0)
+
+@export var coords: Array[Vector2i] = []
 @export var layer: int = 0
 var layer_parent: LayerMenuRow = null
 var dragged: bool = false
@@ -19,7 +13,6 @@ var activated: bool = false
 
 func _process(_delta: float) -> void:
 	if hovered and Input.is_action_just_pressed("click") and not dragged:
-		print("[MASK]: Inviamo")
 		dragged = true
 		mask_dragged.emit(true, self)
 	if Input.is_action_just_released("click") and dragged:
@@ -29,12 +22,6 @@ func _process(_delta: float) -> void:
 
 ## Ritorna le coordinate rispetto alla griglia del livello che la maschera copre
 func get_mask_coords() -> Array[Vector2i]:
-	var coords: Array[Vector2i] = []
-	
-	for row in range(shape.size()):
-		for column in shape[row]:
-			coords.push_back(Vector2i(coord_position.x + row, coord_position.y + column))
-	
 	return coords
 
 
