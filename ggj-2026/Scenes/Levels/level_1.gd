f72f5e5821d6422da0ec73529bb266e90a619136
AdrianoRean <127209495+AdrianoRean@users.noreply.github.com>
2026-02-01 03:35:20 +0100

    fix leve 1

--- ggj-2026/Scenes/Levels/level_1.gd
+++ ggj-2026/Scenes/Levels/level_1.gd
@@ -36,6 +36,6 @@ func _ready() -> void:
 		
 	pass # Replace with function body.
 
-
 func _on_exit_area_body_entered(body: Node2D) -> void:
+	SignalBus.emit_signal("game_over")
 	pass # Replace with function body.
