ad2f0f83730cda5d9689b8684a2e21a1af006341
jfabi88 <jacopo.fabi8@gmail.com>
2026-05-04 17:09:38 +0200

    Fix problemi vari livello 2

--- ggj-2026/scenes/game/components/obj/dang_obj.gd
+++ ggj-2026/scenes/game/components/obj/dang_obj.gd
@@ -6,5 +6,14 @@ func _ready() -> void:
 	$StaticBody2D.body_entered.connect(_on_body_entered)
 
 
+func _apply_disable(value: bool) -> void:
+	if value:
+		hide()
+		$StaticBody2D.monitoring = false
+	else:
+		show()
+		$StaticBody2D.monitoring = true
+
+
 func _on_body_entered(body: Node2D) -> void:
 	body.game_over()
--- ggj-2026/scenes/game/components/sensor_area.gd
+++ ggj-2026/scenes/game/components/sensor_area.gd
@@ -1,25 +1,42 @@
 class_name SensorArea extends Area2D
 
-
-signal covered
-
+@export var node_to_disable: GameObj
 var is_covered: bool = false
-var above_objects = 0
+var to_check: bool = false
+var counter: int = 0
 
 
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
+	if node_to_disable:
+		node_to_disable.shape_changed.connect(_on_shape_changed)
 
 
-func _on_body_entered(_body: Node2D) -> void:
-	above_objects += 1
-	is_covered = true
-	covered.emit()
+func _physics_process(_delta: float) -> void:
+	if to_check:
+		var value = is_covered_now()
+		if not is_covered and value:
+			is_covered = true
+			node_to_disable._apply_disable(true)
+		elif is_covered and not value:
+			is_covered = false
+			node_to_disable._check_and_apply_disable()
+		counter += 1
+		if counter >= 4:
+			to_check = false
 
 
-func _on_body_exited(_body: Node2D) -> void:
-	above_objects -= 1
-	if above_objects <= 0:
-		is_covered = false
-		covered.emit()
+func is_covered_now() -> bool:
+	var space_state = get_world_2d().direct_space_state
+	
+	var query = PhysicsShapeQueryParameters2D.new()
+	query.shape = $CollisionShape2D.shape
+	query.transform = $CollisionShape2D.global_transform
+	query.collision_mask = collision_mask
+	query.collide_with_bodies = true
+
+	return space_state.intersect_shape(query).size() > 0
+
+
+func _on_shape_changed() -> void:
+	to_check = true
+	counter = 0
