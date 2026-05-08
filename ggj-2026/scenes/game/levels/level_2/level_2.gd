5bd18434817b173657650a9d1f20321f3e407893
jfabi88 <jacopo.fabi8@gmail.com>
2026-05-08 23:19:30 +0200

    boh

---
+++ ggj-2026/scenes/game/levels/level_2/level_2.gd
@@ -0,0 +1,14 @@
+extends Level
+
+
+func _on_conchiglia_area_body_entered(body: Node2D) -> void:
+	body.deactivate()
+	$SubViewport/Layers/Layer0/GameObj/ConchigliaArea.set.call_deferred("monitoring", false)
+	$SubViewport/Layers/Layer0/GameObj/Conchiglia.play("Door")
+	$SubViewport/Layers/Layer0/GameObj/Conchiglia.animation_finished.connect(_on_animation_finished)
+
+
+func _on_animation_finished():
+	$SubViewport/Layers/Layer0/GameObj/Door/StaticBody2D.monitoring = true
+	$SubViewport/Layers/Layer0/GameObj/Door.show()
+	player.activate()
